a2d68a2bec5e36d8b1cb348c1dd97f2747b6b27f
Zoe Berthold <zoeberthold@zoes-macbook-pro.local>
2018-07-23 23:11:42 -0500

    when you click, the block goes... somewhere

--- drawStuff/ViewController.swift
+++ drawStuff/ViewController.swift
@@ -9,9 +9,13 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet weak var blueSquare: UIView!
+    @IBOutlet weak var pixelBlock: UILabel!
     override func viewDidLoad() {
+        
         super.viewDidLoad()
+        print("hey")
         // Do any additional setup after loading the view, typically from a nib.
     }
 
@@ -19,7 +23,38 @@ class ViewController: UIViewController {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
+    
+    @IBAction func onTappedSquare(_ sender: UITapGestureRecognizer) {
+        if blueSquare.frame.contains(sender.location(in: blueSquare)) {
+            var point = sender.location(in: blueSquare)
+            pixelBlock.center = point
+            print(point)
+            //pixelBlock.center = sender.location(in: blueSquare)
+        }
+        if blueSquare.frame.contains(sender.location(ofTouch: 0, in: blueSquare)){
+            print("idk what this does")
+        }
+    }
+    
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        for touch in touches {
+            let location = touch.location(in: blueSquare)
+            pixelBlock.center = location
+        }
+    }
+    
+    override func touchesMoved(_ touches: Set<UITouch>, with event: UIEvent?) {
+        for touch in touches {
+            let location = touch.location(in: blueSquare)
+            pixelBlock.center = location
+        }
+    }
+    
+//    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+//        for touch in touches {
+//            print("yup")
+//        }
+//    }
 
 }
 
