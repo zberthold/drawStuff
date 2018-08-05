d9b20c67afb0e9eea686c312b25d80ecc3c04bda
Zoe Berthold <zoeberthold@zoes-macbook-pro.local>
2018-08-05 14:10:30 -0500

    brick moves where you click within the label

--- drawStuff/ViewController.swift
+++ drawStuff/ViewController.swift
@@ -10,13 +10,16 @@ import UIKit
 
 class ViewController: UIViewController {
     
+    @IBOutlet weak var testLabel: UILabel!
     @IBOutlet weak var blueSquare: UIView!
     @IBOutlet weak var pixelBlock: UILabel!
+    var labelsArray = [UILabel]()
     override func viewDidLoad() {
         
         super.viewDidLoad()
         print("hey")
         // Do any additional setup after loading the view, typically from a nib.
+        labelsArray.append(testLabel)
     }
 
     override func didReceiveMemoryWarning() {
@@ -25,30 +28,40 @@ class ViewController: UIViewController {
     }
     
     @IBAction func onTappedSquare(_ sender: UITapGestureRecognizer) {
-        if blueSquare.frame.contains(sender.location(in: blueSquare)) {
-            var point = sender.location(in: blueSquare)
-            pixelBlock.center = point
-            print(point)
-            //pixelBlock.center = sender.location(in: blueSquare)
-        }
-        if blueSquare.frame.contains(sender.location(ofTouch: 0, in: blueSquare)){
-            print("idk what this does")
+//        if blueSquare.frame.contains(sender.location(in: blueSquare)) {
+//            var point = sender.location(in: blueSquare)
+//            pixelBlock.center = point
+//            print(point)
+//            //pixelBlock.center = sender.location(in: blueSquare)
+//        }
+//        if blueSquare.frame.contains(sender.location(ofTouch: 0, in: blueSquare)){
+//            print("idk what this does")
+//        }
+        for label in labelsArray {
+            if label.frame.contains(sender.location(in: blueSquare)){
+                label.text = "O"
+            }
         }
     }
     
     override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
         for touch in touches {
-            let location = touch.location(in: blueSquare)
+            var location = touch.location(in: blueSquare)
+            print(location)
+            location.x += 37.5
+            location.y += 183.5
             pixelBlock.center = location
+            print(pixelBlock.center)
         }
     }
     
     override func touchesMoved(_ touches: Set<UITouch>, with event: UIEvent?) {
         for touch in touches {
-            let location = touch.location(in: blueSquare)
+            var location = touch.location(in: blueSquare)
             pixelBlock.center = location
         }
     }
+    
     
 //    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
 //        for touch in touches {
